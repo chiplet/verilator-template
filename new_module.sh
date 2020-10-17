58a1acb7f35ee908e7d71058c13a8bdf32549d20
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2020-10-17 07:43:09 +0300

    Add template project generator script.

---
+++ new_module.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 project-name"
+    exit 1
+fi
+
+MODULENAME=$1
+
+cp -r TEMPLATE $MODULENAME
+cd $MODULENAME && \
+    mv MODULENAME.v $MODULENAME.v && \
+    mv tb_MODULENAME.cpp tb_$MODULENAME.cpp
+    sed -i '' "s/FILE=MODULENAME/FILE=$MODULENAME/g" Makefile
+    sed -i '' "s/MODULENAME/$MODULENAME/g" $MODULENAME.v tb_$MODULENAME.cpp
+
